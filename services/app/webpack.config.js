389f921318657d2cdf35c21a86f9cdef28129039
Michaelangelo Jong <mike96jong@gmail.com>
2019-08-26 16:06:17 -0300

    add file-loader to app

--- services/app/webpack.config.js
+++ services/app/webpack.config.js
@@ -15,6 +15,10 @@ module.exports = {
         test: /\.s?css$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
     ],
   },
   mode: 'development',
